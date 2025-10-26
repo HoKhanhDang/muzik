67f08a5aa690a4bc6b5f8d405184ac21042674aa
hokhanhdang <hokhanhdang203@gmail.com>
2025-10-26 15:53:54 +0700

    add cors

--- muzik/server/server.js
+++ muzik/server/server.js
@@ -16,7 +16,13 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }
+));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
